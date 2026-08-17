3851de71423900269239adf3221da5762f752d92
IftachShafriri <iftachs2001@gmail.com>
2026-08-17 11:50:42 +0300

    ALU UPDATE

--- ALU.v
+++ ALU.v
@@ -1,30 +1,61 @@
 module ALU #(parameter WIDTH = 4) (a, b, y, carry, op, overflow, zero, shift_amount);
 
 input [WIDTH - 1 : 0] a, b ;
-input [3 : 0] op ;
-input [1 : 0] shift_amount ;
+input [4 : 0] op ;
+input [ceil(log2(WIDTH)) : 0] shift_amount ;
 output reg [WIDTH - 1 : 0] y ;
 output reg carry, overflow ;
-output reg zero ;
-// Setting Parameter OPCODE
-parameter OP_ADD = 4'b0000 ;
-parameter OP_SUB = 4'b0001 ;
-parameter OP_AND = 4'b0010 ;
-parameter OP_OR = 4'b0011 ;
-parameter OP_XOR = 4'b0100 ;
-parameter OP_NOT = 4'b0101 ;
-parameter OP_MUL = 4'b0110 ;
-parameter OP_DIV = 4'b0111 ;
-parameter OP_MOD = 4'b1000 ;
-parameter OP_EQ = 4'b1001 ;
-parameter OP_LT = 4'b1010 ;
-parameter OP_Shift_left = 4'b1011 ;
-parameter OP_Shift_rigth = 4'b1100 ;
-parameter OP_Shift_left_sign = 4'b1101 ;
-parameter OP_Shift_rigth_sign = 4'b1111;
+output zero ;
 
+// Setting Parameter OPCODE
+parameter OP_ADD = 5'b00000 ;
+parameter OP_SUB = 5'b00001 ;
+parameter OP_AND = 5'b00010 ;
+parameter OP_OR = 5'b00011 ;
+parameter OP_XOR = 5'b0100 ;
+parameter OP_NOT = 5'b00101 ;
+parameter  OP_Shift_left = 5'b00110 ;
+parameter  OP_Shift_rigth = 5'b00111 ;
+parameter  OP_Shift_rigth_sign = 5'b01000 ;
+parameter  OP_Shift_Cycle= 5'b01001 ;
+parameter OP_LT = 5'b01010 ;
+parameter OP_EQ = 5'b01011 ;
+//parameter  = 5'b01100 ;
+//parameter  = 5'b01101 ;
+//parameter  = 5'b01111;
 
 always @* begin
     carry = 1'b0;
     case(op)
-    3
+    OP_ADD: {carry, y} = a + b ; // ADD, ADDI
+    OP_SUB: y = a - b ; // SUB, SUBI
+    OP_AND: y = a & b ; // AND
+    OP_OR: y = a | b ; // OR
+    OP_XOR: y = a ^ b ; // XOR
+    OP_NOT: y = ~a ; // NOT
+    OP_EQ: y = ($signed(a) == $signed(b)) ; // Equal
+    OP_LT: y = ($signed(a) < $signed(b)) ; // Less then
+    OP_Shift_left: y = a << shift_amount ; // SHL
+    OP_Shift_rigth: y = a >> shift_amount ; // SHR
+    OP_Shift_rigth_sign: y = $signed(a) >>> shift_amount ; // SHR Sign Keeping
+    OP_Shift_Cycle: // SHL Cycle
+        if (shift_amount == 0 )
+            y = a;
+        else 
+            y = (a << shift_amount) | (a >> (WIDTH - shift_amount));
+
+    end
+    default: y = {WIDTH{1'b0}} ;
+    endcase
+end
+
+always @* begin
+    case (op)
+    OP_ADD: overflow = (a[WIDTH-1] == b[WIDTH-1]) && (a[WIDTH-1] != y[WIDTH-1]) ;
+    OP_SUB: overflow = (a[WIDTH-1] != b[WIDTH-1]) && (a[WIDTH-1] != y[WIDTH-1]) ;
+        default: overflow = 1'b0 ; 
+    endcase
+    
+end
+assign zero = (y == {WIDTH{1'b0}}) ;
+endmodule
