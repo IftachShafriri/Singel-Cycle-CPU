011d9745ce8f799007848e4ea9d0f3eaaab8953b
IftachShafriri <iftachs2001@gmail.com>
2026-08-19 16:28:03 +0300

    Saving PC

--- Include/DATA.v
+++ Include/DATA.v
@@ -3,5 +3,7 @@
 
 `define Data_WIDTH 4
 `define Number_of_Registers 64
-
+`define PC_WIDTH 16
+`define INSTRUCTION_WIDTH 32 // 5 for opcode 6 for register a,b,c 9 for const
+`define Register_bit_number ($clog2(64))
 `endif 
